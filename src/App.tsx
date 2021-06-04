c6dddd6adac7edfeade3b676de47c20db9b400b8
johir-rayhan <johir.rayhan01@gmail.com>
2021-06-04 21:11:04 +0600

    Header Web background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import logo from './assets/shopify_logo.png';
-import headerBanner from './assets/img-1.png';
-import './App.css';
 import Header from './components/Header';
 
 function App() {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from '../assets/shopify_logo.png';
+import waveBackground from '../assets/wave_background.svg';
 import headerBanner from '../assets/img-1.png';
 
 function Header() {
     return (
         <section className="site-header">
             <img src={logo} alt="logo" className="logo" />
-            <div>
+            <div className="title">
                 <h1>Sell online with shopify</h1>
                 <p>Trusted by over 1,000,000 businesses worldwide</p>
             </div>
 
             <div className="free-trial-form">
-                <input type="text" className="input" placeholder="Enter your email address" />
+                <input type="text" placeholder="Enter your email address" />
                 <button>Start free trial</button>
             </div>
             <p className="note">
                 Try Shopify free for 14 days, no credit card required. By entering your email, you agree to receive marketing emails from Shopify.
-        </p>
+            </p>
 
             <img src={headerBanner} alt="banner" className="header-banner" />
+            <img src={waveBackground} alt="wave-banner" className="wave" />
         </section>
     );
 }
